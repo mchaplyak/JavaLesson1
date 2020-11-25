dd39329c5bcbf3e694b3ee6143a4e6340c237bd0
chaplyak <m.chapliak@gmail.com>
2020-11-25 22:35:27 +0200

    firstTask

---
+++ src/Main.java
@@ -0,0 +1,114 @@
+public class Main {
+    public static void main(String[] args) {
+//        1. Створити 10 цілочисленних змінних
+//          - додати їх між собою
+//          - відняти їх між собою
+//          - перемножити їх між собою
+//          - поділити їх між собою
+//          Вивести результати в консоль
+
+        int num1 = 12;
+        int num2 = 2;
+        int num3 = 98;
+        int num4 = 259;
+        int num5 = 13;
+        int num6 = 34;
+        int num7 = 442;
+        int num8 = 566;
+        int num9 = 339;
+        int num10 = 476;
+        
+
+
+        int plus = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+        System.out.println("PlusResult: " + plus);
+        int minus = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+        ;
+        System.out.println("MinusResult: " + minus);
+        int multiply = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+        ;
+        System.out.println("MultiplyResult: " + multiply);
+        int divide = num1 / num2;
+        System.out.println("DivideResult: " + divide);
+
+        String firstWord = "Lorem";
+        String secondWord = "ipsum";
+        String thirdWord = "dolor";
+        String fourthWord = "sit";
+        String fiveWord = "amet";
+        char sixChar = ',';
+        String sevenWord = "consectetur";
+        String eightWord = "adipiscing";
+        String nineWord = "elit.";
+
+        String sentence =
+                "MySentence: " +
+                        " " +
+                        firstWord +
+                        " " +
+                        secondWord +
+                        " " +
+                        thirdWord +
+                        " " +
+                        fourthWord +
+                        " " +
+                        fiveWord +
+                        sixChar +
+                        " " +
+                        sevenWord +
+                        " " +
+                        eightWord +
+                        " " +
+                        nineWord;
+        System.out.println(sentence);
+        User user1 = new User(1, 23, "Max", "Chaplyak", 55.5,173);
+        User user2 = new User(2, 21, "Vova", "Petiv", 65.3,175);
+        User user3 = new User(3, 30, "Oleh", "Katola", 76.2,180);
+        User user4 = new User(4, 45, "Segiy", "Formus", 63.7,172);
+        User user5 = new User(5, 18, "Vasyl", "Bobylyak", 74.1,181);
+        User user6 = new User(6, 12, "Pedro", "Schepanyak", 44.4,160);
+        User user7 = new User(7, 65, "Yura", "Bednar", 80.8,185);
+        User user8 = new User(8, 34, "Bodya", "Steciv", 95.5,195);
+        User user9 = new User(9, 26, "Nazar", "Beniv", 58.9,170);
+        User user10 = new User(10, 33, "Andriy", "Geryliv", 59.9,169);
+
+        int agePlus = user1.age +
+                user2.age +
+                user3.age +
+                user4.age +
+                user5.age +
+                user6.age +
+                user7.age +
+                user8.age +
+                user9.age +
+                user10.age;
+
+        System.out.println("AllUserAge: " + agePlus);
+
+        double weightPlus = user1.weight +
+                user2.weight +
+                user3.weight +
+                user4.weight +
+                user5.weight +
+                user6.weight +
+                user7.weight +
+                user8.weight +
+                user9.weight +
+                user10.weight;
+        System.out.println("AllUserWeight: " + weightPlus);
+
+        double heightPlus = user1.height +
+                user2.height +
+                user3.height +
+                user4.height +
+                user5.height +
+                user6.height +
+                user7.height +
+                user8.height +
+                user9.height +
+                user10.height;
+        System.out.println("AllUserHeight: " + heightPlus);
+    }
+
+
+}
---
+++ src/User.java
@@ -0,0 +1,80 @@
+public class User {
+    int id;
+    int age;
+    String name;
+    String surname;
+    double weight;
+    double height;
+
+    public User() {
+    }
+
+    public User(int id, int age, String name, String surname, double weight, double height) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+        this.surname = surname;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", age=" + age +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", weight=" + weight +
+                ", height=" + height +
+                '}';
+    }
+}
